9709e36a6c5847cb908e59067b4b27cf9a1f885e
juanjose <juanjolo12@hotmail.com>
2023-09-19 09:49:42 -0500

    feat(forms): Date picker added to the create student view

--- MSDE_App/forms.py
+++ MSDE_App/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class CreateStudent(forms.Form):
     code = forms.CharField(label="Student's code", max_length=24, widget=forms.TextInput())
     name = forms.CharField(label="Student's name", max_length=40)
-    birth_date = forms.DateField(label="Birth Date (YYYY-MM-DD)")
+    birth_date = forms.DateField(widget=DateInput(attrs={'type': 'date'}))
     id = forms.CharField(label="Student's id", max_length=12)
     email = forms.CharField(label="Student's mail", max_length=40)
     phone_number = forms.CharField(label="Student's phone number", max_length=40)
--- MSDE_App/urls.py
+++ MSDE_App/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('index/', views.index, name="index"),
-    path('student/create_student/', views.create_student, name="create_student/"),
+    path('student/create_student/', views.create_student, name="create_student"),
     path('students/', views.students, name="students/"),
     path('student/<int:student_code>/edit/', views.edit_student, name='edit_student'),
     path('student/<int:student_code>/delete/', views.delete_student, name='delete_student')
