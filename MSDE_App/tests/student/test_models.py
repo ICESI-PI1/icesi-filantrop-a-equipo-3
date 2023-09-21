6e4463b81521e4d955b42d2de9435253141ccbc2
juanjose <juanjolo12@hotmail.com>
2023-09-20 19:15:59 -0500

    feat(test): add more test cases for Student model

--- MSDE_App/tests/student/test_models.py
+++ MSDE_App/tests/student/test_models.py
@@ -26,12 +26,24 @@ class StudentTestCase(TestCase):
                                student_birth_date='2016-08-03',
                                student_phone_number='314123',
                                donor_student_code=don)
+        Student.objects.create(student_code='A0037129',
+                               student_id='100392',
+                               student_name='Sara',
+                               student_email='sara@sara.com',
+                               student_ICFES_score='389',
+                               student_birth_date='2019-08-03',
+                               student_phone_number='9393',
+                               donor_student_code=don)
 
     def test_student_exist(self):
         juan = Student.objects.get(student_code='A00381190')
-        pablo = Student.objects.get(student_code='A0039869')
         self.assertEquals(juan.student_name, 'Juan Jose')
+
+        pablo = Student.objects.get(student_code='A0039869')
         self.assertEquals(pablo.student_name, 'Pablo')
+
+        sara = Student.objects.get(student_code='A0037129')
+        self.assertEquals(sara.student_name, 'Sara')
 
     def test_student_relationship(self):
         juan = Student.objects.get(student_code='A00381190')
@@ -41,6 +53,10 @@ class StudentTestCase(TestCase):
         pablo = Student.objects.get(student_code='A0039869')
         self.assertEquals(pablo.donor_student_code.donor_name, 'patricio')
         self.assertEquals(pablo.donor_student_code.donor_code, '1')
+
+        sara = Student.objects.get(student_code='A0037129')
+        self.assertEquals(sara.donor_student_code.donor_name, 'patricio')
+        self.assertEquals(sara.donor_student_code.donor_code, '1')
 
     def test_unique_constraint(self):
         student2 = Student(
@@ -55,3 +71,16 @@ class StudentTestCase(TestCase):
         )
         with self.assertRaises(IntegrityError):
             student2.save()
+
+        student3 = Student(
+            student_code="A0037129",
+            student_name="Bob",
+            student_birth_date="2001-01-01",
+            student_id="2222222222",
+            student_email="bob@example.com",
+            student_phone_number="0987654321",
+            student_ICFES_score=600,
+            donor_student_code=None
+        )
+        with self.assertRaises(IntegrityError):
+            student3.save()
