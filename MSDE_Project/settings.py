d67bffbf89a8541c7999ecb2cc9e1d39506ad02c
JuanJoseLL <1001367985@u.icesi.edu.co>
2023-10-11 12:49:05 -0500

    update: add postgresql db

--- MSDE_Project/settings.py
+++ MSDE_Project/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'MSDE_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django",
+        "USER": "juan",
+        "PASSWORD": "Msde_app",
+        "HOST": "msde-app-db.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"}
     }
 }
 
