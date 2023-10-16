8f2f99634475e16d5a66fc3ec91dad0d16523217
JuanJoseLL <1001367985@u.icesi.edu.co>
2023-10-16 09:53:54 -0500

    fix: now selenium works with github actions v2

--- MSDE_App/tests/student/test_end_to_end.py
+++ MSDE_App/tests/student/test_end_to_end.py
@@ -17,7 +17,9 @@ class TestCreateStudent(LiveServerTestCase):
         password_input = driver.find_element(By.XPATH,
                                              '/html/body/section/div/div/div/div/div/div[1]/div/form/div[2]/label[2]/input')
         password_input.send_keys('123')
-        driver.find_element(By.XPATH, '/html/body/section/div/div/div/div/div/div[1]/div/form/div[3]/button').click()
+
+        password_input.submit()
+
 
         time.sleep(2)
 
