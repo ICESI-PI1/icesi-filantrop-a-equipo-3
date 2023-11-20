c63c4cb5984aec23df140c60b0aa20d94beb9fa5
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-11-20 02:19:53 -0500

    fix(initial-data): Change normal pwds to hash pwds in order to provide a correct login

--- MSDE_App/views/home_view.py
+++ MSDE_App/views/home_view.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from MSDE_Collaborator_App import *
 from django.template.loader import render_to_string
+from django.contrib.auth.hashers import make_password
 
 is_superuser = False
+
+
+def makepwd(request):
+    password = "integrador"
+    hashed_password = make_password(password)
+    print(hashed_password)
+    return render(request, 'index.html')
 
 @login_required
 def index(request):
